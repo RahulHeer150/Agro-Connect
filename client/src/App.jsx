6d486ed4b5ed9b80343f3a4cddc52138f5c40172
Rahul Heer <rahulheer344@gmail.com>
2025-12-25 22:47:22 +0530

    new RouterSetup for Project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,55 @@ import { useState } from 'react'
 import './App.css'
 import "./index.css"
 import Navbar from './components/Navbar'
+import { Router, Routes,Route } from 'react-router-dom'
+import Register from './components/Register'
+import CartItem from './components/CartItem'
+import CartSummary from './components/CartSummary'
+import Login from './components/Login'
+import AuthPage from './pages/AuthPage'
+import Cart from './pages/Cart'
+import Home from './pages/Home'
+import Payment from './pages/Payment'
+import PaymentSuccess from './components/PaymentSuccess'
+import PaymentFailure from './components/PaymentFailure'
+import AddProduct from './components/AddProduct'
+import Dashboard from './components/Dashboard'
+import MyProduct from './components/MyProduct'
+import MyOrders from './components/MyOrders'
+import FarmerDashBoard from './pages/FarmerDashBoard'
+Ma
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Navbar/>
+    <> 
+    <Navbar/>
+    <Routes>
+      <Route path="/register" element={<Register/>}/>
+      <Route path='/authpage' element={<AuthPage/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/payment' element={<Payment/>}/>
+      <Route path='/payment-success' element={<PaymentSuccess/>}/>
+      <Route path='/payment-failure' element={<PaymentFailure/>}/>
+      <Route path='/addProduct' element={<AddProduct/>}/>
+      <Route path='/cart-item' element={<CartItem/>}/>
+      <Route path='/cart-summary' element={<CartSummary/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/my-orders' element={<MyOrders/>}/>
+      <Route path='/my-product' element={<MyProduct/>}/>
+      <Route path='/farmer-dashboard' element={<FarmerDashBoard/>}/>
+      <Route path='/buyer-dashboard' element={<BuyerDashBoard/>}/>
+      <Route path='/marketplace' element={<MarketPlace/>}/>
+      <Route path='/product-details' element={<ProductDetails/>}/>
+
+    </Routes>
+     
+
     </>
   )
 }
