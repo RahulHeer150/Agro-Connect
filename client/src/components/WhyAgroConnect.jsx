d74c824a6fcb71c5aa1b2461660ece9998231814
Rahul Heer <rahulheer344@gmail.com>
2025-12-28 21:33:12 +0530

    new components for homepage

---
+++ client/src/components/WhyAgroConnect.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhyAgroConnect = () => {
+  return (
+    <div>WhyAgroConnect</div>
+  )
+}
+
+export default WhyAgroConnect
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import HowItWorks from '../components/HowItWorks'
+import WhyAgroConnect from '../components/WhyAgroConnect'
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
+      <HowItWorks/>
+      <WhyAgroConnect/>
     </>
   )
 }
