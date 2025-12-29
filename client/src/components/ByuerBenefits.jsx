10db04dc519e1a59547c0ed34ea657c75f698989
Rahul Heer <rahulheer344@gmail.com>
2025-12-29 12:33:31 +0530

    new compoennet for homepage

---
+++ client/src/components/ByuerBenefits.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaLeaf, FaMapMarkedAlt, FaBalanceScale  } from 'react-icons/fa'
+
+
+const ByuerBenefits = () => {
+  return (
+<section className='bg-green-50 py-20'>
+    <div className='max-w-7xl mx-auto px-6 text-center'>
+        <h2 className='text-3xl font-bold mb-12'>
+            Benefits for Buyers
+        </h2>
+    </div>
+
+</section>
+  )
+}
+
+export default ByuerBenefits
