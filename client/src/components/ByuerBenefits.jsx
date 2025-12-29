e192b74495ce247254c10f9e12ec7264c68e8f82
Rahul Heer <rahulheer344@gmail.com>
2025-12-29 12:38:41 +0530

    new final compoennent

--- client/src/components/ByuerBenefits.jsx
+++ client/src/components/ByuerBenefits.jsx
@@ -9,6 +9,26 @@ const ByuerBenefits = () => {
         <h2 className='text-3xl font-bold mb-12'>
             Benefits for Buyers
         </h2>
+
+        <div className='grid md:grid-cols-3 gap-10'>
+            <div className='bg-white p-6 rounded-xl shadow-sm'>
+                <FaLeaf className='text-green-700 text-4xl mx-auto mb-4'/>
+                <h3 className='font-semibold'>Fresh Produce</h3>
+
+            </div>
+
+            <div className='bg-white p-6 rounded-xl shadow-sm'>
+                <FaMapMarkedAlt className='text-green-700 text-4xl mx-auto mb-4'/>
+                <h3 className='font-semibold'>Local Farmers</h3>
+
+            </div>
+
+            <div className='bg-white p-6 rounded-xl shadow-sm'>
+                <FaBalanceScale className='text-green-700 text-4xl mx-auto mb-4'/>
+                <h3 className='font-semibold'>Fair Pricing</h3>
+
+            </div>
+        </div>
     </div>
 
 </section>
