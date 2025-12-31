5f12eb2754681ee310f755aba42320595c8ab591
Rahul Heer <rahulheer344@gmail.com>
2025-12-31 11:16:19 +0530

    new cartItem file code

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const CartItem = () => {
+const CartItem = ({item}) => {
   return (
-    <div>CartItem</div>
+    <div className='bg-white p-5 rounded-xl shadow-sm flex justify-between items-center'>
+      <div>
+        <h3 className='font-semibold text-lg'>
+          {item.name}
+        </h3>
+        <p className='text-sm text-gray-600'>
+          Farmer:{item.farmer}
+        </p>
+
+      </div>
+      <p className='font-semibold text-gray-700'>
+{item.price}
+      </p>
+    </div>
+   
   )
 }
 
