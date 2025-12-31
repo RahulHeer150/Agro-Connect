cd37bc9d4b1e5de96633b2a48e3703cb73a808ee
Rahul Heer <rahulheer344@gmail.com>
2025-12-31 11:38:43 +0530

    new cartsummary basic UI code

--- client/src/components/CartSummary.jsx
+++ client/src/components/CartSummary.jsx
@@ -15,8 +15,24 @@ const CartSummary = () => {
         Price Summary
 
       </h3>
-      CartSummary</div>
+      <div className='flex justify-between text-sm mb-2'>
+        <span>
+          Total Items
+        </span>
+        <span>{cart.length}</span>
+      </div>
+
+      <div className='flex justfy-between font-semibold text-lg mb-6'>
+      <span>Total</span>
+      <span>₹{totalPrice}</span>
+      </div>
+
+      <button className='w-full bg-green-700 text-white py-3 rounded-lg hover:bg-green-800 transition'>
+Proceed to CheckOut
+      </button>
+
+      </div>
   )
 }
 
-export default CartSummary
+export default CartSummary;
