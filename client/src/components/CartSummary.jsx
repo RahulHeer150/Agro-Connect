93c0fc839c63a03ec33a2a17b5a46ed7c7539635
Rahul Heer <rahulheer344@gmail.com>
2025-12-31 11:29:44 +0530

    new cart code

--- client/src/components/CartSummary.jsx
+++ client/src/components/CartSummary.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useCart } from '../context/CartContext'
+
 
 const CartSummary = () => {
+  const {cart}=useCart();
+
+  const totalPrice=cart.reduce(
+    (sum,item)=>sum+item.price,0
+    )
   return (
-    <div>CartSummary</div>
+    <div className='bg-white p-6 rounded-xl shadow-sm h-fit'>
+      
+      <h3 className='font-semibold text-lg mb-4'>
+        Price Summary
+
+      </h3>
+      CartSummary</div>
   )
 }
 
