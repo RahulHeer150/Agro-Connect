1897aed328bb9458441b5c63bcd0facd529a05d6
Rahul Heer <rahulheer344@gmail.com>
2026-01-04 18:55:29 +0530

    debugging for error

--- Backend/controllers/paymentcontroller.js
+++ Backend/controllers/paymentcontroller.js
@@ -7,7 +7,6 @@ const razorpay=new Razorpay({
     key_secret:process.env.RAZORPAY_KEY_SECRET,
 })
 
-
 module.exports.createRazorpayOrder=async(req,res)=>{
     try{
         const {orderId}=req.body;
