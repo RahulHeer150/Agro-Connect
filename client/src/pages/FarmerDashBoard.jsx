58c7ca04830050da3d426e2a4bb24e5b5907fefc
Rahul Heer <rahulheer344@gmail.com>
2026-01-05 19:43:46 +0530

    new farmer dashboard

--- client/src/pages/FarmerDashBoard.jsx
+++ client/src/pages/FarmerDashBoard.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import AddProduct from '../components/AddProduct'
 import MyProduct from '../components/MyProduct'
 import Order from '../components/Order'
+import Dashboard from '../components/Dashboard'
 
 const FarmerDashBoard = () => {
   return (
     <div>
+      <Dashboard/>
         <AddProduct/>
         <MyProduct/>
         <Order/>
