c84fcea8f907d5fb0d73eb7ab3ff22885351004d
Rahul Heer <rahulheer344@gmail.com>
2026-01-07 23:09:10 +0530

    file name debugging for frontend

--- client/src/pages/FarmerDashBoard.jsx
+++ client/src/pages/FarmerDashBoard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AddProduct from '../components/AddProduct'
 import MyProduct from '../components/MyProduct'
-import Order from '../components/Order'
+import Orders from '../components/Orders'
 import Dashboard from '../components/Dashboard'
 
 const FarmerDashBoard = () => {
@@ -10,7 +10,7 @@ const FarmerDashBoard = () => {
       <Dashboard/>
         <AddProduct/>
         <MyProduct/>
-        <Order/>
+        <Orders/>
     </div>
   )
 }
