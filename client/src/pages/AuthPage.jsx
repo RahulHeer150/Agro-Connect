4eacdb0566a0fbfc8e831c4d6a2094d1b8ee0363
Rahul Heer <rahulheer344@gmail.com>
2026-01-09 23:09:48 +0530

    new basic code changes

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Login from '../components/Login'
-import Register from '../components/Register'
+import React from "react";
+import Login from "../components/Login";
+import Register from "../components/Register";
 
 const AuthPage = () => {
   return (
     <div>
-        <Login/>
-        <Register/>
+      <Login />
+      <Register />
     </div>
-  )
-}
+  );
+};
 
 export default AuthPage;
