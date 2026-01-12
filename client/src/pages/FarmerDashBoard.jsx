a6d6257659885012f3232533c7daad9968504b2b
Rahul Heer <rahulheer344@gmail.com>
2026-01-12 22:56:21 +0530

    new code changes

--- client/src/pages/FarmerDashBoard.jsx
+++ client/src/pages/FarmerDashBoard.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import AddProduct from '../components/AddProduct'
-import MyProduct from '../components/MyProduct'
-import Orders from '../components/Orders'
-import Dashboard from '../components/Dashboard'
+import React from "react";
+import AddProduct from "../components/AddProduct";
+import MyProduct from "../components/MyProduct";
+import Orders from "../components/Orders";
+import Dashboard from "../components/Dashboard";
 
 const FarmerDashBoard = () => {
   return (
     <div>
-      <Dashboard/>
-        <AddProduct/>
-        <MyProduct/>
-        <Orders/>
+      <Dashboard />
+      <AddProduct />
+      <MyProduct />
+      <Orders />
     </div>
-  )
-}
+  );
+};
 
-export default FarmerDashBoard
+export default FarmerDashBoard;
