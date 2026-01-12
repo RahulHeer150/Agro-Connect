907da0a0109eac61e08dd7e50f3688bb78b4d4c4
Rahul Heer <rahulheer344@gmail.com>
2026-01-12 22:56:43 +0530

    new updations for component calling and debugging

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import PaymentSuccess from '../components/PaymentSuccess'
-import PaymentFailure from '../components/PaymentFailure'
+import React from "react";
+import PaymentSuccess from "../components/PaymentSuccess";
+import PaymentFailure from "../components/PaymentFailure";
 
 const Payment = () => {
   return (
-    <div>Payment
-        <PaymentSuccess/>
-        <PaymentFailure/>
+    <div>
+      Payment
+      <PaymentSuccess />
+      <PaymentFailure />
     </div>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
