8caa5413b3eca482ce00f1619f04de2089eba8c4
Rahul Heer <rahulheer344@gmail.com>
2026-01-14 20:42:39 +0530

    new code changes

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -5,7 +5,7 @@ const ContactUs = () => {
   const submithandler = (e) => {
     e.preventDefault();
   };
-  
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-green-700 to-sky-50">
       <div className="bg-white rounded-xl shadow-lg w-[400px] p-8 shadow-green-800">
@@ -22,18 +22,19 @@ const ContactUs = () => {
             className="bg-[#eeeeee] mb-4 rounded-lg px-4 py-2 border w-full"
           />
           <textarea
-           type="text"
-            placeholder="Enter your Feedback here" 
-            rows={5} 
+            type="text"
+            placeholder="Enter your Feedback here"
+            rows={5}
             cols={20}
             className="bg-[#eeeeee] mb-4 rounded-lg px-4 py-2 border w-full"
-            />
+          />
 
-            <button onClick={submithandler} 
-            className="w-full border-gray-100 rounded-lg mb-4 px-4 py-2 text-white font-semibold bg-green-700">
-              Submit your Feedback
-
-            </button>
+          <button
+            onClick={submithandler}
+            className="w-full border-gray-100 rounded-lg mb-4 px-4 py-2 text-white font-semibold bg-green-700"
+          >
+            Submit your Feedback
+          </button>
         </form>
       </div>
     </div>
