3c1719f55689a32737ad31e8647fe2d5ba2959c8
Rahul Heer <rahulheer344@gmail.com>
2026-01-16 22:34:24 +0530

    new routes code updations

--- Backend/routes/paymentroutes.js
+++ Backend/routes/paymentroutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { createRazorpayOrder, verifyRazorpayPayment } = require("../controllers/paymentcontroller");
+const {
+  createRazorpayOrder,
+  verifyRazorpayPayment,
+} = require("../controllers/paymentcontroller");
 const { authUser, authorizeRoles } = require("../middlewares/authmiddleware");
 
-router.post("/razorpay/create", authUser, authorizeRoles("buyer"), createRazorpayOrder);
-router.post("/razorpay/verify", authUser, authorizeRoles("buyer"), verifyRazorpayPayment);
+router.post(
+  "/razorpay/create",
+  authUser,
+  authorizeRoles("buyer"),
+  createRazorpayOrder
+);
+router.post(
+  "/razorpay/verify",
+  authUser,
+  authorizeRoles("buyer"),
+  verifyRazorpayPayment
+);
 
 module.exports = router;
