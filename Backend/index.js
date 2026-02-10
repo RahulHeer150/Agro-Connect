d39ae342d92622785ac9dcf54bf83b9792aaae47
Rahul Heer <rahulheer344@gmail.com>
2026-02-10 21:39:31 +0530

    new code changes

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const connectToDb = require("./config/db");
 dotenv.config();
 
 const app = express();
-]
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
