6766e2d9c57d774a86f759e8bc4072dc60dbd608
Rahul Heer <rahulheer344@gmail.com>
2026-03-23 01:10:36 +0530

    new productgrid code with api to upload dynamic data from backend to frotnend

--- client/src/components/ProductGrid.jsx
+++ client/src/components/ProductGrid.jsx
@@ -1,57 +1,15 @@
 import ProductCard from "./ProductCard";
 
-const products = [
-  {
-    name: "Tomatoes",
-    price: 40,
-    category: "Vegetables",
-    farmer: "Ramesh Kumar",
-  },
-  {
-    name: "Potatoes",
-    price: 30,
-    category: "Vegetables",
-    farmer: "Suresh Patil",
-  },
-  {
-    name: "Wheat",
-    price: 55,
-    category: "Grains",
-    farmer: "Mahesh Yadav",
-  },
-  {
-    name: "Apples",
-    price: 80,
-    category: "Fruits",
-    farmer: "Amit Sharma",
-  },
-];
-
-const ProductGrid = ({ filters }) => {
-  const filteredProducts = products.filter((p) => {
-    if (
-      filters.categories.length > 0 &&
-      !filters.categories.includes(p.category)
-    ) {
-      return false;
-    }
-
-    if (filters.price === "low" && p.price >= 30) return false;
-    if (filters.price === "mid" && (p.price < 30 || p.price > 50)) return false;
-    if (filters.price === "high" && p.price <= 50) return false;
-
-    return true;
-  });
+const ProductGrid = ({ products }) => {
+  if (products.length === 0) {
+    return <p>No products found</p>;
+  }
 
   return (
-    <div className="lg:col-span-3 grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
-      {filteredProducts.length > 0 ? (
-        filteredProducts.map((p, index) => (
-          <ProductCard key={index} product={p} />
-        ))
-      ) : (
-        <p className="text-gray-500 col-span-full">No products found.</p>
-      )}
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
+      {products.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
     </div>
   );
 };
