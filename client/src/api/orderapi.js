26c43b859ea689379ae6a3c87f5a94aee5cc3020
Rahul Heer <rahulheer344@gmail.com>
2026-03-24 12:27:49 +0530

    new order api to fetch my orders data

--- client/src/api/orderapi.js
+++ client/src/api/orderapi.js
@@ -2,4 +2,10 @@ import api from "./axios";
 
 export const placeOrderAPI = (orderData) => {
   return api.post("/order/create", orderData);
+
+
+};
+
+export const getMyOrdersAPI=()=>{
+  return api.get("/api/orders/my")
 };
