d8da483421d387e641caafbf048f55f543250f3b
Rahul Heer <rahulheer344@gmail.com>
2026-03-29 18:17:04 +0530

    new code for loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const Loader = () => {
+const Loader = ({ size = "medium" }) => {
+  const sizes = {
+    small: "w-5 h-5 border-2",
+    medium: "w-8 h-8 border-4",
+    large: "w-12 h-12 border-4",
+  };
+
   return (
-    <div>Loader</div>
-  )
-}
+    <div className="flex justify-center items-center py-6">
+      <div
+        className={`animate-spin rounded-full border-green-700 border-t-transparent ${sizes[size]}`}
+      ></div>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
