846b7e95d52a5b89fc56f3d70f8fc414435d8fe4
Rahul Heer <rahulheer344@gmail.com>
2026-03-29 22:34:10 +0530

    new distance helper code for distance calculations

--- server/utils/distanceHelper.js
+++ server/utils/distanceHelper.js
@@ -1,6 +1,3 @@
-const const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+exports.kmToMeters= (km)=>{
+    return km*1000;
+}
