95057c4205dc401a8d481a1448219985edb911a4
Rahul Heer <rahulheer344@gmail.com>
2026-03-30 22:05:22 +0530

    new code with tailwind styling

--- client/src/components/LocationButton.jsx
+++ client/src/components/LocationButton.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const LocationButton = ({onClick}) => {
+const LocationButton = ({ onClick }) => {
   return (
-    <button onClick={onClick}>
-      Use My Current Location
+    <button
+      onClick={onClick}
+      className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg shadow-md transition duration-200"
+    >
+      📍 Use My Location
     </button>
-  )
-}
+  );
+};
 
-export default LocationButton
+export default LocationButton;
