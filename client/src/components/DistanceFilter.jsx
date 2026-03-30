81236629b012d1ce6e700c35f47951e7f39002a6
Rahul Heer <rahulheer344@gmail.com>
2026-03-30 22:05:06 +0530

    adding styling using tailwind

--- client/src/components/DistanceFilter.jsx
+++ client/src/components/DistanceFilter.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const DistanceFilter = ({setDistance}) => {
+const DistanceFilter = ({ setDistance }) => {
   return (
-    <div>DistanceFilter</div>
-  )
-}
+    <div className="flex gap-3 mt-3">
+      {[10, 30, 50].map((km) => (
+        <button
+          key={km}
+          onClick={() => setDistance(km)}
+          className="px-3 py-1 bg-gray-200 hover:bg-green-500 hover:text-white rounded-md transition"
+        >
+          {km} KM
+        </button>
+      ))}
+    </div>
+  );
+};
 
-
-export default DistanceFilter
+export default DistanceFilter;
