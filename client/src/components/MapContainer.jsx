b32519e52c8e9f576bc75fc8ab3d21a8160239b5
Rahul Heer <rahulheer344@gmail.com>
2026-03-30 22:22:31 +0530

    mapcontainer code for frontend

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
+import Loader from './Loader';
+import FarmerMarker from './FarmerMarker';
 
-const MapContainer = () => {
+
+const MapContainer = ({location,farmers}) => {
+
+  if(!location){
+    return   <p className='text-center mt-10'> <Loader/>Loading map...</p>
+  }
   return (
-    <div>MapContainer</div>
+    <div className='w-full h-[500px] rounded-xl overflow-hidden shadow-lg mt-5'>
+      <LeafletMap
+      center={[location.lat,location.lng]}
+      zoom={13}
+      className='w-full h-full'
+      >
+        <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; OpenStreetMap contributors'
+      />
+
+      {farmers.map((farmer)=>(
+        <FarmerMarker key={farmer._id} farmer={farmer}/>
+      ))}
+      </LeafletMap>
+
+    </div>
   )
 }
 
