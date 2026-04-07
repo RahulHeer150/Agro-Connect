8cf79bccb07441230be224fa3489775e156d610a
Rahul Heer <rahulheer344@gmail.com>
2026-04-07 21:45:38 +0530

    new code updations for frontend

--- server/services/geoService.js
+++ server/services/geoService.js
@@ -1,16 +1,19 @@
 const User = require("../models/usermodel");
 
 exports.findNearbyFarmers = async (lng, lat, maxDistance) => {
-  return await User.find({
-    role: "farmer", // 🔥 IMPORTANT
-    location: {
-      $near: {
-        $geometry: {
-          type: "Point",
-          coordinates: [lng, lat], // [lng, lat]
-        },
-        $maxDistance: maxDistance,
+ return await User.find({
+  role: "farmer",
+  location: { $exists: true },
+  "location.coordinates.0": { $exists: true },
+  "location.coordinates.1": { $exists: true },
+  location: {
+    $near: {
+      $geometry: {
+        type: "Point",
+        coordinates: [lng, lat],
       },
+      $maxDistance: maxDistance,
     },
-  }).select("-password"); // hide password
+  },
+}).select("-password");
 };
