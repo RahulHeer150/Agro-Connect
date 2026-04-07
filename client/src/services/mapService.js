c2550c456054a7392a37fcf981c7af51073ef310
Rahul Heer <rahulheer344@gmail.com>
2026-04-07 11:14:35 +0530

    new code updations

--- client/src/services/mapService.js
+++ client/src/services/mapService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const getNearbyFarmers= async (lat, lng, distance)=>{
-    const res= await axios.get(
-`/api/map/nearby?lat=${lat}&lng=${lng}&distance=${distance}`
-    );
-    return res.data.data;
+export const getNearbyFarmers = async (lat, lng, distance) => {
+  const res = await axios.get("/api/map/nearby", {
+    params: { lat, lng, distance },
+  });
 
-}
+  return res.data.data; // ✅ IMPORTANT (return array only)
+};
