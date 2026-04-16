34f9bdbca509603cba04dbe376639266dcb086f3
Rahul Heer <rahulheer344@gmail.com>
2026-04-16 12:40:23 +0530

    new code updations for server email functionality

--- server/controllers/ordercontroller.js
+++ server/controllers/ordercontroller.js
@@ -1,7 +1,6 @@
 const Cart = require("../models/cartmodel");
 const Order = require("../models/ordermodel");
 const Product = require("../models/productmodel");
-const {notifyFarmer}
 
 module.exports.placeOrder = async (req, res) => {
   try {
