54664ec4c406ea96089e25591ffc6f941bf8be25
Rahul Heer <rahulheer344@gmail.com>
2026-05-31 19:02:09 +0530

    new pages for admin functionaloty

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Navigate} from 'react-router-dom'
+import {useAuth} from '../context/AuthContext'
+import Loader from '../components/Loader'
+
+const ProtectedRoute = () => {
+    const {use,isLoggedIn,loading}  = useAuth();
+
+    if(loading){
+        return <Loader size='large'/>
+    }
+
+    if(!isLoggedIn){
+        return <Navigate to='/login' replace/>
+    }
+
+    if(user?.role !== 'admin'){
+        return <Navigate to='/' replace/>
+    }
+
+    return children;
+
+
+  
+}
+
+export default AdminProtectedRoute;
