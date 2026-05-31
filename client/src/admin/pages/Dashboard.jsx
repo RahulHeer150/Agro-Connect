4cefdee719a6e62e59666178a9e89afed253802c
Rahul Heer <rahulheer344@gmail.com>
2026-06-01 00:31:19 +0530

    fetching data and displaying onscreen

--- client/src/admin/pages/Dashboard.jsx
+++ client/src/admin/pages/Dashboard.jsx
@@ -1,31 +1,56 @@
 import { Package, ShoppingCart, Tractor, Users } from 'lucide-react'
 import React from 'react'
 import StatCard from '../components/dashboard/StatCard'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { getDashboardStats } from '../services/AdminService'
 
 const Dashboard = () => {
+  const[stats, setStats]=useState({
+    farmers:0,
+    buyers:0,
+    products:0,
+    orders:0,
+  })
 
-  const stats=[
+  useEffect(()=>{
+    fetchStats();
+  },[]);
+
+  const fetchStats=async()=>{
+    try {
+      const data= await getDashboardStats();
+      setStats(data);
+      
+    } catch (error) {
+
+      console.error(error.message);
+      
+    }
+  }
+
+  const data=[
     {
       title:"Total Farmers",
-      value:0,
+      value:stats.farmers,
       icon:Tractor,
       color:"bg-green-600"
     },
     {
       title:"Total Buyers",
-      value:0,
+      value:stats.buyers,
       icon:Users,
       color:"bg-green-600"
     },
     {
       title:"Total Products",
-      value:0,
+      value:stats.products,
       icon:Package,
       color:"bg-green-600"
     },
     {
       title:"Total Orders",
-      value:0,
+      value:stats.orders,
       icon:ShoppingCart,
       color:"bg-green-600"
     },
@@ -37,7 +62,7 @@ const Dashboard = () => {
       </h1>
 
       <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6'>
-        {stats.map((item)=>(
+        {data.map((item)=>(
           <StatCard key={item.title}
           {...item}
           />
--- client/src/admin/services/AdminService.js
+++ client/src/admin/services/AdminService.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const getDashboardStats=async()=>{
-    const res= await axios.get
+    const res= await axios.get(
+        "https://agro-connect-8yjz.onrender.com"
+    );
+    return res.data;
 }
