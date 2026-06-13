e502580e4a4cb9dfd4d11b2ad183260b4fe099b1
Rahul Heer <rahulheer344@gmail.com>
2026-06-13 23:32:16 +0530

    new code functionality for notification service panel

--- client/src/services/notificationService.js
+++ client/src/services/notificationService.js
@@ -1,31 +1,49 @@
 import axios from "axios";
 
-const API=  "http://localhost:5000/api/notifications";
+const API = "http://localhost:5000/api/notifications";
 
+export const getMyNotifications = async () => {
+  const token = localStorage.getItem("token");
 
-export const getMyNotifications=async()=>{
-    const token= localStorage.getItem("token");
-
-    const res= await axios.get(
-        `${API}/my-notifications`,
-        {
-            headers:{
-                Authorization:`Bearer ${token}`,
-            }
-        }
-    )
-
-    return res.data;
-}
-
-export const getUnreadCount=async()=>{
-    const token = localStorage.getItem("token"),
+  const res = await axios.get(
+    `${API}/my-notifications`,
     {
-        headers:{
-            Authorization:`Bearer ${token}`,
-
-        }
-        
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
+  );
 
-}
+  return res.data;
+};
+
+export const getUnreadCount = async () => {
+  const token = localStorage.getItem("token");
+
+  const res = await axios.get(
+    `${API}/unread-count`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return res.data;
+};
+
+export const markNotificationRead = async (id) => {
+  const token = localStorage.getItem("token");
+
+  const res = await axios.put(
+    `${API}/${id}/read`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return res.data;
+};
