501f1b24ce86b70b6327d00da3ca2f6cf8e3f7ac
Rahul Heer <rahulheer344@gmail.com>
2026-06-14 22:50:17 +0530

    new code updations for bel icons

--- client/src/admin/components/layout/Navbar.jsx
+++ client/src/admin/components/layout/Navbar.jsx
@@ -1,31 +1,27 @@
 import { useAuth } from "../../../context/AuthContext";
+import { NotificationBell } from "../../../components/NotificationBell";
 
-const Navbar=()=>{
-    const {user} = useAuth();
+const Navbar = () => {
+  const { user } = useAuth();
 
-    return (
-        <header className="h-16 bg-white shadow px-6 flex items-center justify-between">
-            <h2 className="font-semibold text-2xl">
-                Admin Dashboard
+  return (
+    <header className="h-16 bg-white shadow px-6 flex items-center justify-between">
+      <h2 className="font-semibold text-2xl">Admin Dashboard</h2>
 
-            </h2>
+      <div className="flex items-center gap-3">
+        <NotificationBell />
+        <div className="w-10 h-10 rounded-full bg-green-700 flex items-center justify-center text-white">
+          {user?.name?.charAt(0)}
+        </div>
 
-            <div className="flex items-center gap-3">
-                <div className="w-10 h-10 rounded-full bg-green-700 flex items-center justify-center text-white">
-                    {user?.name?.charAt(0)}
-                </div>
+        <div>
+          <p className="font-medium">{user?.name}</p>
 
-                <div>
-                    <p className="font-medium">{user?.name}</p>
-
-                    <p className="text-sm text-gray-500">
-                        Administrator
-                    </p>
-                </div>
-            </div>
-
-        </header>
-    )
-}
+          <p className="text-sm text-gray-500">Administrator</p>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
