d9f861a379b3d9fe3100634098c51b03207bb3d2
Rahul Heer <rahulheer344@gmail.com>
2026-06-16 09:48:53 +0530

    new code chnages for frontend

--- client/src/admin/components/layout/Navbar.jsx
+++ client/src/admin/components/layout/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
 
 
 
-
       <div className="flex items-center gap-3">
         <NotificationBell />
         <div className="w-10 h-10 rounded-full bg-green-700 flex items-center justify-center text-white">
