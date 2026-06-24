6febec5fc3a7dec211d044751dc2451dc7894ae2
Rahul Heer <rahulheer344@gmail.com>
2026-06-24 22:53:17 +0530

    new code chnages

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -17,6 +17,11 @@ const Profile = () => {
   const navigate = useNavigate();
   const completion = calculateProfileCompletion(user);
 
+
+
+
+
+  
   const fetchProfile = async () => {
     try {
       const res = await api.get("/api/auth/profile");
